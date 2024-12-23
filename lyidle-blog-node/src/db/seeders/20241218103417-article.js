76600c0afa43925187d3d1b9037622b1280d9cd4
槑 <79448635+lyidle@users.noreply.github.com>
2024-12-23 18:51:20 +0800

    initial

--- lyidle-blog-node/src/db/seeders/20241218103417-article.js
+++ lyidle-blog-node/src/db/seeders/20241218103417-article.js
@@ -12,26 +12,44 @@ module.exports = {
     // 循环次数 和 用户的种子对应
     const counts = 50
     // 模拟分类
-    const category = ["前端", "后端", "软件", "游戏"]
+    const category = [
+      "前端",
+      "后端",
+      "软件",
+      "游戏",
+      "test88888888888888888888",
+    ]
     // 模拟标签
-    const tips = ["html", "css", "js"]
+    const tags = ["html", "css", "js", "test88888888888888888888"]
     // 模拟用户
     for (let i = 1; i <= counts; i++) {
       author.push(`test${i}`)
+    }
+    // 模拟描述
+    const desc = (i) => {
+      if (i === 1 || i === counts)
+        return `文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容文章的描述内容${i}`
+      else return `文章的描述内容文章的描述内容文章的描述内容文章的描述内容${i}`
+    }
+    // 模拟title
+    const title = (i) => {
+      if (i === 1 || i === counts)
+        return `文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的title文章的ti${i}`
+      else return `文章的title文章的title文章的title文章的title${i}`
     }
     // 模拟文章数据
     for (let i = 1; i <= counts; i++) {
       const curAuthor = author[Math.floor(Math.random() * author.length)]
       const content = `文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容文章的内容 ${i}`
       const article = {
-        title: `文章的标题 ${i}`,
+        title: title(i),
         content,
         author: curAuthor,
         category: category[Math.floor(Math.random() * category.length)],
-        tags: JSON.stringify(tips),
+        tags: JSON.stringify(tags[Math.floor(Math.random() * tags.length)]),
         userId: Number(curAuthor.replace("test", "")),
         carousel: [0, 1][Math.floor(Math.random() * 2)],
-        desc: `文章的描述内容文章的描述内容文章的描述内容文章的描述内容${i}`,
+        desc: desc(i),
         length: content.length,
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -39,7 +57,7 @@ module.exports = {
       articles.push(article)
       userInfo.push({
         pages: 1,
-        tags: JSON.stringify([...new Set(tips)]),
+        tags: JSON.stringify([article.tags]),
         categories: JSON.stringify([article.category]),
         userId: article.userId,
         totalWords: `${content.length}`,
